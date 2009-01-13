8c9f3e64556af8f76c65905f82a8f0bf34be4790
Bryce Kerley <bryce@worldmedia.net>
2009-01-13 16:42:37 -0500

    no dox, just codes that works

--- ccrb-watcher.rb
+++ ccrb-watcher.rb
@@ -1,8 +1,10 @@
 #!/usr/bin/env ruby
+require 'rubygems'
 require 'rss/2.0'
 require 'open-uri'
+require 'active_support'
 
-class Project
+class Build
   attr_accessor :status, :id, :date
   def initialize(rss_item)
     self.date = rss_item.date
@@ -12,7 +14,7 @@ class Project
   end
   
   def opinion
-    return "bad" if successful?
+    return "bad" if !successful?
     return "meh" if stale?
     return "ok"
   end
@@ -25,24 +27,42 @@ class Project
     date < 1.week.ago
   end
   
-  def to_growlnotify
-    "growlnotify -n cc.rb --image cc-icon_#{opinion}.png -m \"#{id} at #{date} is #{opinion}\" \"#{opinion} for you!\""
+  def growlnotify
+    cmd = "growlnotify -n cc.rb --image cc-icon_#{opinion}.png -m - \"#{opinion} for you!\""
+    message = "#{id} at #{date.localtime.strftime "%c"} is #{opinion}"
+    IO.popen cmd, 'w' do |c|
+      c.puts message
+    end
   end
 end
 
 class Watcher
-  attr_accessor :uri
+  attr_accessor :uri, :builds
   def initialize(uri)
     self.uri = uri
+    self.builds = {}
   end
   
-  def last_project
-    Project.new(rss.items[0])
+  def check_last_build
+    b = Build.new(@rss.items[0])
+    new_build b if !seen_build?(b)
   end
   
-  private
-  def rss
-    @rss ||= fetch
+  def new_build(b)
+    builds[b.id] = b
+    b.growlnotify
+  end
+  
+  def seen_build?(b)
+    builds.include? b.id
+  end
+  
+  def watch
+    while true
+      fetch
+      check_last_build
+      sleep 60*10
+    end
   end
   
   def fetch
@@ -51,6 +71,19 @@ class Watcher
       content = s.read
     end
     
-    RSS::Parser.parse(content, false)
+    @rss = RSS::Parser.parse(content, false)
   end
+end
+
+begin
+  if $0 == __FILE__
+    url = ARGV[0]
+    w = Watcher.new(url)
+    w.watch
+  end
+rescue Exception => e
+  $stderr.puts "barf!"
+  $stderr.puts e.backtrace.join("\n")
+  $stderr.puts e.inspect
+  exit -1
 end
